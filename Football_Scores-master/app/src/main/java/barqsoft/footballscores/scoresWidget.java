5f24ddea7063a2f4b4956640044c6c9d62f1acc9
sjaak-mb <sjaak@mobilebridges.com>
2015-12-17 10:18:44 +0100

    first commit

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresWidget.java
@@ -0,0 +1,62 @@
+package barqsoft.footballscores;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import barqsoft.footballscores.service.myScoresWidgetRemoveViewsService;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+@TargetApi(14)
+public class scoresWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        }
+    }
+
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.scores_list_widget);
+
+        views.setTextViewText(R.id.scores_list_title, widgetText);
+        Intent intent = new Intent(context, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.scores_list_title, pendingIntent);
+
+        views.setRemoteAdapter(R.id.scores_list, new Intent(context, myScoresWidgetRemoveViewsService.class));
+        views.setEmptyView(R.id.scores_list, R.id.scores_empty);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
+
---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/service/myScoresWidgetRemoveViewsService.java
@@ -0,0 +1,144 @@
+package barqsoft.footballscores.service;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import barqsoft.footballscores.DatabaseContract;
+import barqsoft.footballscores.R;
+import barqsoft.footballscores.Utilies;
+
+/**
+ * Created by sjaakkallemein on 15/12/15.
+ */
+@TargetApi(11)
+public class myScoresWidgetRemoveViewsService extends RemoteViewsService {
+
+    private interface ScoresQuery {
+        String[] COLUMNS = {
+                DatabaseContract.scores_table.ID_COL,
+                DatabaseContract.scores_table.DATE_COL,
+                DatabaseContract.scores_table.TIME_COL,
+                DatabaseContract.scores_table.HOME_COL,
+                DatabaseContract.scores_table.AWAY_COL,
+                DatabaseContract.scores_table.LEAGUE_COL,
+                DatabaseContract.scores_table.HOME_GOALS_COL,
+                DatabaseContract.scores_table.AWAY_GOALS_COL,
+                DatabaseContract.scores_table.MATCH_ID,
+                DatabaseContract.scores_table.MATCH_DAY,
+        };
+    }
+
+    public static final int COL_ID = 0;
+    public static final int COL_DATE = 1;
+    public static final int COL_TIME = 2;
+    public static final int COL_HOME = 3;
+    public static final int COL_AWAY = 4;
+    public static final int COL_LEAGUE = 5;
+    public static final int COL_HOME_GOALS = 6;
+    public static final int COL_AWAY_GOALS = 7;
+    public static final int COL_MATCH_ID = 8;
+    public static final int COL_MATCH_DAY = 9;
+
+    @Override
+    public RemoteViewsService.RemoteViewsFactory onGetViewFactory(Intent intent) {
+
+        return new RemoteViewsFactory() {
+            private Cursor cursor = null;
+
+            @Override
+            public void onCreate() {
+            }
+
+            @Override
+            public void onDataSetChanged() {
+
+                final long identity = Binder.clearCallingIdentity();
+
+                Uri uri = DatabaseContract.scores_table.buildScoreWithDate();
+
+                String todayDate = this.DateString(3);
+
+                cursor = getContentResolver().query(uri, ScoresQuery.COLUMNS, null, new String[]{todayDate}, null);
+
+                Binder.restoreCallingIdentity(identity);
+            }
+
+            protected String DateString(int daysBack) {
+                Date fragmentdate = new Date(System.currentTimeMillis()-(daysBack*86400000));
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                return format.format(fragmentdate);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (cursor != null) {
+                    cursor.close();
+                    cursor = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                if (cursor == null)
+                    return  0;
+                return cursor.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                    cursor == null ||
+                    !cursor.moveToPosition(position)) {
+                    return null;
+                }
+                final RemoteViews views = new RemoteViews(getPackageName(), R.layout.scores_list_item);
+
+                String score = Utilies.getScores(cursor.getInt(COL_HOME_GOALS), cursor.getInt(COL_AWAY_GOALS));
+
+                views.setTextViewText(R.id.data_textview, cursor.getString(COL_TIME));
+                views.setTextViewText(R.id.home_name, cursor.getString(COL_HOME));
+                views.setTextViewText(R.id.away_name, cursor.getString(COL_AWAY));
+                views.setTextViewText(R.id.score_textview, score);
+
+                views.setImageViewResource(R.id.home_crest, Utilies.getTeamCrestByTeamName(cursor.getString(COL_HOME)));
+                views.setImageViewResource(R.id.away_crest, Utilies.getTeamCrestByTeamName(cursor.getString(COL_AWAY)));
+
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.scores_list_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (cursor.moveToPosition(position))
+                    return cursor.getLong(1);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+
+    }
+}
